bf6aa9fc9ef161e0e7fda3df2b1e3b47fcac916a
Auxence <audel22@student.sdu.dk>
2023-01-31 15:26:21 +0100

    Like/Dislike

--- app/src/main/java/fr/isen/pulse/pulsenetwork/DetailActivity.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/DetailActivity.kt
@@ -1,12 +1,18 @@
 package fr.isen.pulse.pulsenetwork
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import fr.isen.pulse.pulsenetwork.classes.Post
 import fr.isen.pulse.pulsenetwork.databinding.ActivityDetailBinding
 
+
 class DetailActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityDetailBinding
+
+	var mIvToggle: ImageView? = null
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -17,7 +23,22 @@ class DetailActivity : AppCompatActivity() {
 		val actionBar = supportActionBar
 		actionBar?.title = post?.titre
 
+		binding.auteurPost.text = post.auteur
+		binding.descriptionPost.text = post.description
+		binding.titrePost.text = post.titre
 
+/*
+		binding.ivToggle?.setOnClickListener {
+			//fun onClick(view: View?) {
+				mIvToggle!!.isActivated = !mIvToggle!!.isActivated
+			//}
+		}*/
+
+
+		/*
+		val image = post.image
+		if (image != "") { if (image != null) { Picasso.get().load(image).into(binding.imagePost) }}
+		*/
 
 
 
