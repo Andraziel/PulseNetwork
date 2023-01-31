29a92eaa38b777e92587a24d71c66d4bee8d0e6c
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-01-31 11:31:07 +0100

    restauration

--- app/src/main/java/fr/isen/pulse/pulsenetwork/PostActivity.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/PostActivity.kt
@@ -1,11 +1,42 @@
 package fr.isen.pulse.pulsenetwork
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fr.isen.pulse.pulsenetwork.databinding.ActivityMainBinding
+import fr.isen.pulse.pulsenetwork.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
+	private lateinit var binding: ActivityPostBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_post)
+		binding = ActivityPostBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+
+
+		// titrePost, imagePost, descriptionPost, closePost
+		binding.closePost.setOnClickListener {
+			val intent = Intent(this, FeedActivity::class.java)
+			startActivity(intent)
+		}
+
+		binding.validePost.setOnClickListener {
+			val image = binding.imagePost
+			val titre = binding.titrePost.text.toString()
+			val description = binding.descriptionPost.text.toString()
+
+			val database = Firebase.database("https://pulsenetwork-d6541-default-rtdb.europe-west1.firebasedatabase.app")
+			val myRef = database.getReference("pulse/post")
+			myRef.push()
+
+			myRef.push().setValue("Hello, World2!")
+			Log.w("-------------", "Value is: " + myRef)
+		}
+
+
 	}
 }
