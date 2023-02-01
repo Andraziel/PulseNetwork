fa5ec459abdc2f9289f57aa5d133481e79c7ab1f
Auxence <audel22@student.sdu.dk>
2023-02-01 23:39:38 +0100

    Added a loooot of things including the like buttons

--- app/src/main/java/fr/isen/pulse/pulsenetwork/DetailActivity.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/DetailActivity.kt
@@ -2,7 +2,12 @@ package fr.isen.pulse.pulsenetwork
 
 import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.BounceInterpolator
+import android.view.animation.ScaleAnimation
+import android.widget.CompoundButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import fr.isen.pulse.pulsenetwork.classes.Post
@@ -28,6 +33,43 @@ class DetailActivity : AppCompatActivity() {
 		binding.titrePost.text = post.titre
 		binding.nbLikes.text = post.like.toString()
 		binding.nbDislikes.text = post.dislike.toString()
+
+		// Animation for toggle button
+		var scaleAnimation = ScaleAnimation(
+			0.7f,
+			1.0f,
+			0.7f,
+			1.0f,
+			Animation.RELATIVE_TO_SELF,
+			0.7f,
+			Animation.RELATIVE_TO_SELF,
+			0.7f
+		)
+		scaleAnimation?.setDuration(500)
+		var bounceInterpolator = BounceInterpolator()
+		scaleAnimation?.setInterpolator(bounceInterpolator)
+
+		// Toggle Button Code
+		val toggle_like = binding.likePost
+		toggle_like.setOnCheckedChangeListener(object:View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+			override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+				p0?.startAnimation(scaleAnimation) //To change body of created functions use File | Settings | File Templates.
+				if (p1) {
+					// The toggle is enabled
+					binding.nbLikes.text = (binding.nbLikes.text.toString().toInt()?.plus(1)).toString()
+
+				} else {
+					// The toggle is disabled
+					binding.nbLikes.text = (binding.nbLikes.text.toString().toInt()?.minus(1)).toString()
+				}
+			}
+
+			override fun onClick(p0: View?) {
+				TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			}
+		})
+
+
 /*
 		binding.ivToggle?.setOnClickListener {
 			//fun onClick(view: View?) {
