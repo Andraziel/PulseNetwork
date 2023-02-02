1e1535b80e82971238d3c92dd607af1a5da3e5c9
Auxence <audel22@student.sdu.dk>
2023-02-02 19:48:05 +0100

    Rapidly changed some things so that it's really the name of the author that written with the post, which was not the case before

--- app/src/main/java/fr/isen/pulse/pulsenetwork/PostActivity.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/PostActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import fr.isen.pulse.pulsenetwork.classes.Post
+import fr.isen.pulse.pulsenetwork.classes.UserInfo
 import fr.isen.pulse.pulsenetwork.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityPostBinding
+	private val uid = FirebaseAuth.getInstance().currentUser?.uid
+	private lateinit var fullName: String
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_post)
@@ -24,6 +32,18 @@ class PostActivity : AppCompatActivity() {
 			startActivity(intent)
 		}
 
+		// Getting the user name from the database
+		val link = Firebase.database("https://pulsenetwork-d6541-default-rtdb.europe-west1.firebasedatabase.app").getReference("pulse/users/$uid")
+		link.addListenerForSingleValueEvent(object: ValueEventListener {
+			override fun onDataChange(snapshot: DataSnapshot) {
+				val user = snapshot.getValue<UserInfo>()
+				fullName = user?.firstName + " " + user?.lastName
+			}
+			override fun onCancelled(error: DatabaseError) {
+				Log.w("TAG", "Failed to read value.", error.toException())
+			}
+		})
+
 		binding.validePost.setOnClickListener {
 			val image = binding.imagePost.text.toString()
 			val titre = binding.titrePost.text.toString()
@@ -34,7 +54,7 @@ class PostActivity : AppCompatActivity() {
 			val database = Firebase.database("https://pulsenetwork-d6541-default-rtdb.europe-west1.firebasedatabase.app")
 			val myRef = database.getReference("pulse/posts")
 			val id = myRef.push().key
-			val post = Post(description, id, image, titre, 0, 0, "auteur")
+			val post = Post(description, id, image, titre, 0, 0, fullName)
 			id?.let {
 				myRef.child(it).setValue(post)
 			}
