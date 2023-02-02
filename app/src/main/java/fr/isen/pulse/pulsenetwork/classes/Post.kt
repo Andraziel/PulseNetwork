f5bf36cf198600b894763eae958cedc2443ccb8b
Auxence <audel22@student.sdu.dk>
2023-02-02 20:06:58 +0100

    Cleaned some s

--- app/src/main/java/fr/isen/pulse/pulsenetwork/classes/Post.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/classes/Post.kt
@@ -14,6 +14,5 @@ data class Post (
   @SerializedName("auteur"       ) var auteur       : String? = null,
   @SerializedName("likes"       ) var likes       : ArrayList<String>? = arrayListOf(),
   @SerializedName("likes"       ) var dislikes       : ArrayList<String>? = arrayListOf()
-  //@SerializedName("commentaries"       ) var commentaries       : ArrayList<Commentaire>? = arrayListOf()
 
 ):java.io.Serializable
