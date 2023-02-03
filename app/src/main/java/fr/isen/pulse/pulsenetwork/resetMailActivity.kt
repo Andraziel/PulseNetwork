dc4fd4c299a0e7538fecb7bfdc865a22e038588e
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-02-03 13:41:43 +0100

    tiens je push

--- app/src/main/java/fr/isen/pulse/pulsenetwork/resetMailActivity.kt
+++ app/src/main/java/fr/isen/pulse/pulsenetwork/resetMailActivity.kt
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import fr.isen.pulse.pulsenetwork.databinding.ActivityResetMdpBinding
 
-class resetMdp : AppCompatActivity() {
+class ResetMailActivity : AppCompatActivity() {
     private val userId = FirebaseAuth.getInstance().currentUser?.uid
 
     public lateinit var binding: ActivityResetMdpBinding
@@ -20,7 +18,6 @@ class resetMdp : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val link = Firebase.database("https://pulsenetwork-d6541-default-rtdb.europe-west1.firebasedatabase.app").getReference("pulse/users/$userId")
         val user = FirebaseAuth.getInstance().currentUser
 
 
@@ -29,15 +26,15 @@ class resetMdp : AppCompatActivity() {
         val conf = binding.nnouveau.text
         binding.change.setOnClickListener() {
 
-                user?.updateEmail(nouvmail.toString())
-                    ?.addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d("TAG", "User email address updated.")
-                        }
+            user?.updateEmail(nouvmail.toString())
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("TAG", "User email address updated.")
                     }
-                FirebaseAuth.getInstance().signOut()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                }
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
         }
 
